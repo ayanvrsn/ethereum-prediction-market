81f8093520f2b75dae0367977718842e1393e313
Rikatsu17 <daniyaladilbekov@gmail.com>
2026-05-17 12:03:56 +0500

    chore: format code with forge fmt

--- test/PredictAIToken.t.sol
+++ test/PredictAIToken.t.sol
@@ -3,11 +3,9 @@ pragma solidity ^0.8.24;
 
 import "forge-std/Test.sol";
 
-import {PredictAIToken}
-    from "src/token/PredictAIToken.sol";
+import {PredictAIToken} from "src/token/PredictAIToken.sol";
 
 contract PredictAITokenTest is Test {
-
     PredictAIToken token;
 
     address alice = address(1);
@@ -17,33 +15,22 @@ contract PredictAITokenTest is Test {
     }
 
     function testInitialSupply() public {
-        assertEq(
-            token.totalSupply(),
-            1_000_000 ether
-        );
+        assertEq(token.totalSupply(), 1_000_000 ether);
     }
 
     function testMint() public {
-
         token.mint(alice, 100 ether);
 
-        assertEq(
-            token.balanceOf(alice),
-            100 ether
-        );
+        assertEq(token.balanceOf(alice), 100 ether);
     }
 
     function testVotingPower() public {
-
         token.transfer(alice, 100 ether);
 
         vm.prank(alice);
 
         token.delegate(alice);
 
-        assertEq(
-            token.getVotes(alice),
-            100 ether
-        );
+        assertEq(token.getVotes(alice), 100 ether);
     }
 }
